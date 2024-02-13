1553cda4e939b265d4bd93465d88aab33e022131
Juan Segundo Riera <rierajuanse@gmail.com>
2024-02-13 10:56:26 -0300

    Finished social media buttons implementation

--- src/components/landing/about-me/social-media/social-media.tsx
+++ src/components/landing/about-me/social-media/social-media.tsx
@@ -6,26 +6,46 @@ export const SocialMedia = component$(() => {
 	useStyles$(styles);
 	return (
 		<div class="social__links">
-			<div class="social__link social__link-linkedin">
+			<a
+				href="https://www.linkedin.com/in/juan-segundo-riera/"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="social__link social__link-linkedin"
+			>
 				<svg class="social__link-icon">
 					<use xlink:href="images/sprite.svg#icon-linkedin"></use>
 				</svg>
-			</div>
-			<div class="social__link social__link-twitter">
+			</a>
+			<a
+				href="https://twitter.com/RieraJuanse"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="social__link social__link-twitter"
+			>
 				<svg class="social__link-icon">
 					<use xlink:href="images/sprite.svg#icon-x"></use>
 				</svg>
-			</div>
-			<div class="social__link social__link-instagram">
+			</a>
+			<a
+				href="https://www.instagram.com/juanseriera/"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="social__link social__link-instagram"
+			>
 				<svg class="social__link-icon">
 					<use xlink:href="images/sprite.svg#icon-instagram"></use>
 				</svg>
-			</div>
-			<div class="social__link social__link-github">
+			</a>
+			<a
+				href="https://github.com/JuanseRiera"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="social__link social__link-github"
+			>
 				<svg class="social__link-icon">
 					<use xlink:href="images/sprite.svg#icon-github"></use>
 				</svg>
-			</div>
+			</a>
 		</div>
 	);
 });
